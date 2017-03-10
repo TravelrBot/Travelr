71547c3ac1783e5c52ea3cf3e9638f1cd9ae7085
TraceT51 <trace@tschida.us>
2017-03-10 11:10:08 -0600

    Add App Secrets

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "ff6b4beb-ee93-4e58-87ae-4cdc7d52a67b",
+    appPassword: "4VGq7jLMxiDxDBwoYefSrfg"
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());

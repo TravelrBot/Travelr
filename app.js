2635a17b0468354e8287ef277b942a5119137c76
Tracet51 <tracetschida@dilbert.lbj.utexas.edu>
2017-05-24 14:10:51 -0500

    Env Changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 // Create chat bot 
 var connector = new builder.ChatConnector({
-    appId: MICROSOFT_APP_ID,
+    appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
